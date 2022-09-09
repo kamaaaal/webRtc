5a93dd81774fa669172ff75c0bdb797d617f0cf3
kamaaaal <itskamalmohamed@gmail.com>
2022-09-08 23:20:50 -0700

    done 6

--- index2.js
+++ index2.js
@@ -1,6 +1,7 @@
 async function main () {
     const localStream = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
     const localVideo = document.querySelector('#video1')
+    localVideo.srcObject = localStream;
     const pc = new RTCPeerConnection({'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]});
     const offer = await pc.createOffer({'offerToReceiveAudio':true,'offerToReceiveVideo':true});
     pc.addEventListener('icecandidate', event => {
@@ -16,13 +17,17 @@ async function main () {
     const remoteDesc = new RTCSessionDescription(answer);
     await pc.setRemoteDescription(remoteDesc);
     
-    pc.addEventListener('connectionstatechange', event => {
-        if (pc.connectionState === 'connected') {
-            // Peers connected!
-            console.log('peers connectec');
-        }
+    pc.addEventListener('track', async (event) => {
+        console.log(event.streams,"stremas");
+        const [remoteStream] = event.streams;
+        remoteVideo.srcObject = remoteStream;
     });
 
+    pc.ontrack =  async (event) => {
+        console.log(event.streams,"stremas");
+        const [remoteStream] = event.streams;
+        remoteVideo.srcObject = remoteStream;
+    };
 
     // const localStream = await getUserMedia({vide: true, audio: true});
     // const peerConnection = new RTCPeerConnection(iceConfig);
@@ -32,11 +37,6 @@ async function main () {
     });
 
     const remoteVideo = document.querySelector('#video2');
-
-    pc.addEventListener('track', async (event) => {
-        const [remoteStream] = event.streams;
-        remoteVideo.srcObject = remoteStream;
-    });
 
 }
 main();
